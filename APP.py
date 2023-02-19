6073c9df69ce147c92a6ea7d11c07313b19f64b9
noamroy <80029502+noamroy@users.noreply.github.com>
2023-02-19 18:08:00 +0200

    start creating forms

--- APP.py
+++ APP.py
@@ -1,21 +1,27 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
-from kivy.properties import StringProperty
-import os
+from kivy.properties import StringProperty, NumericProperty
 
 ##screen list
-from login import Login
-from connected import Connected
+from gui.login import Login
+from gui.connected import Connected
+from gui.selectdataset import SelectDataset
+from gui.insertquery import InsertQuery
 
 class AnomalabApp(App):
     username = StringProperty(None)
     password = StringProperty(None)
+    usertype = NumericProperty(None)
+    datasetname = StringProperty(None)
+    datasetID = NumericProperty(None)
 
     def build(self):
         manager = ScreenManager()
 
         manager.add_widget(Login(name='login'))
         manager.add_widget(Connected(name='connected'))
+        manager.add_widget(SelectDataset(name='selectdataset'))
+        manager.add_widget(InsertQuery(name='insertquery'))
 
          ##by default return the first widget to be added
         return manager
--- gui/login.py
+++ gui/login.py
@@ -1,20 +1,40 @@
 from kivy.app import App
 from kivy.uix.screenmanager import Screen, SlideTransition
+
+from gui.popup import show_popup
+
 from kivy.lang import Builder
 
-Builder.load_file('login.kv')
+## IMPORT USERS CONTROLLER
+
+Builder.load_file('gui/login.kv')
 
 class Login(Screen):
 
     def do_login(self, loginText, passwordText):
+        if (loginText == '' or passwordText == ''):
+            self.resetForm()
+            show_popup("Must enter user name and password")
+            return
+        
         app = App.get_running_app()
         app.username = loginText
         app.password = passwordText
 
-        print ('name: '+ app.username+' ,pass: '+ app.password)
-
-        self.manager.transition = SlideTransition(direction="left")
-        self.manager.current = 'connected'
+        ##PUT LOGIN FUNCTION
+        ## TRY AND CATCH
+        app.usertype = 1
+        if (app.usertype > 0):
+            self.manager.transition = SlideTransition(direction="left")
+            if (app.usertype == 1):
+                self.manager.current = 'selectdataset'
+            elif (app.usertype == 2):
+                self.manager.current = 'connected'
+            elif (app.usertype == 3):
+                self.manager.current = 'connected'
+        else:
+            self.resetForm()
+            show_popup("Login failed")
 
     def resetForm(self):
         self.ids['userName'].text = ""
