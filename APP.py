8ab4020154f11cbd5ddf68e09c8fb2aa7ddc9b4b
noamroy <80029502+noamroy@users.noreply.github.com>
2023-02-21 19:33:32 +0200

    first set up

--- APP.py
+++ APP.py
@@ -1,36 +1,127 @@
-from kivy.app import App
+from kivymd.app import MDApp
 from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
 from kivy.properties import StringProperty, NumericProperty
+from kivy.lang import Builder
 
-##screen list
-from gui.login import Login
-from gui.connected import Connected
-from gui.selectdataset import SelectDataset
-from gui.insertquery import InsertQuery
-from gui.regularresults import RegularResults
-from gui.dataanalystmenu import DataAnalystMenu
-from gui.datasetsmanager import DatasetsManager
+from gui.popup import show_popup
 
-class AnomalabApp(App):
+Builder.load_file('App.kv')
+
+#SCREENS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#---1---
+class Login(Screen):
+    def do_login(self, name, password):
+        if (name == '' or password == ''):
+            self.resetForm()
+            show_popup("Must enter user name and password")
+            return
+    
+        app = MDApp.get_running_app()
+        app.username = name
+        print (f'login: name-{name} pass-{password}')
+
+        ##PUT LOGIN FUNCTION
+        app.usertype = 2
+        if (app.usertype > 0):
+            self.manager.transition = SlideTransition(direction="left")
+            if (app.usertype == 1):
+                self.manager.current = 'selectdataset'
+            elif (app.usertype == 2):
+                self.manager.current = 'dataanalystmenu'
+            elif (app.usertype == 3):
+                pass
+                ##HERE WILL COME MANAGE USERS SCREEN
+        else:
+            self.resetForm()
+            show_popup("Login failed")
+
+    def resetForm(self):
+        self.ids['user_name'].text = ""
+        self.ids['user_pass'].text = ""
+#---2---
+class ChooseDataset(Screen):
+    pass
+#---3---
+class Query(Screen):
+    pass
+#---4---
+class Results(Screen):
+    pass
+#---5---
+class DataAnalystMenu(Screen):
+    pass
+#---6---
+class ManageDatasets(Screen):
+    pass
+#---7---
+class CrudDatasets(Screen):
+    pass
+#---8---
+class ManageDistanceFunctions(Screen):
+    pass
+#---9---
+class CrudDistanceFunctions(Screen):
+    pass
+#---10---
+class UpdateModels(Screen):
+    pass
+#---11---
+class ManageUsers(Screen):
+    pass
+#---12---
+class CrudUser(Screen):
+    pass
+#---13---
+class ChooseModels(Screen):
+    pass
+#---14---
+class AnalystResults(Screen):
+    pass
+# WINDOW MANAGER ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~``
+class AnomalabApp(MDApp):
+    icon = StringProperty('gui/images/logo.jpg')
+    title = StringProperty('Anomalab')
     username = StringProperty(None)
-    password = StringProperty(None)
     usertype = NumericProperty(None)
     datasetname = StringProperty(None)
     datasetID = NumericProperty(None)
-
     def build(self):
-        manager = ScreenManager()
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "BlueGray"
+        WindowManager = ScreenManager()
+        #---1---
+        WindowManager.add_widget(Login(name='login'))
+        #---2---
+        WindowManager.add_widget(ChooseDataset(name='choosedataset'))
+        #---3---
+        WindowManager.add_widget(Query(name='query'))
+        #---4---
+        WindowManager.add_widget(Results(name='results'))
+        #---5---
+        WindowManager.add_widget(DataAnalystMenu(name='dataanalystmenu'))
+        #---6---
+        WindowManager.add_widget(ManageDatasets(name='managedatasets'))
+        #---7---
+        WindowManager.add_widget(CrudDatasets(name='cruddatasets'))
+        #---8---
+        WindowManager.add_widget(ManageDistanceFunctions(name='managedistancefunctions'))
+        #---9---
+        WindowManager.add_widget(CrudDistanceFunctions(name='cruddistancefunctions'))
+        #---10---
+        WindowManager.add_widget(UpdateModels(name='updatemodels'))
+        #---11---
+        WindowManager.add_widget(ManageUsers(name='manageusers'))
+        #---12---
+        WindowManager.add_widget(CrudUser(name='cruduser'))
+        #---13---
+        WindowManager.add_widget(ChooseModels(name='choosemodels'))
+        #---14---
+        WindowManager.add_widget(AnalystResults(name='analystresults'))
+        
+        return WindowManager
 
-        manager.add_widget(Login(name='login'))
-        manager.add_widget(Connected(name='connected'))
-        manager.add_widget(SelectDataset(name='selectdataset'))
-        manager.add_widget(InsertQuery(name='insertquery'))
-        manager.add_widget(RegularResults(name='regularresults'))
-        manager.add_widget(DataAnalystMenu(name='dataanalystmenu'))
-        manager.add_widget(DatasetsManager(name='datasetsmanager'))
-
-         ##by default return the first widget to be added
-        return manager
-
+## MAIN
 if __name__ == '__main__':
-    AnomalabApp().run()
+    app = AnomalabApp()
+    app.run()
+    app.root_window.close()
