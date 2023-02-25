06ac1dad53f32d5bc875051786cb3239555dd49e
noamroy <80029502+noamroy@users.noreply.github.com>
2023-02-25 12:05:37 +0200

    fix data tables sizes

--- APP.py
+++ APP.py
@@ -4,7 +4,7 @@ from kivymd.app import MDApp
 from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
 from kivy.properties import StringProperty, NumericProperty
 from kivy.lang import Builder
-
+from kivy.core.window import Window
 #table imports
 from kivymd.uix.datatables import MDDataTable
 from kivy.metrics import dp
@@ -16,8 +16,6 @@ from kivy.uix.spinner import Spinner
 from gui.popup import show_popup
 
 Builder.load_file('App.kv')
-
-
 
 #SCREENS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #---1---
@@ -32,7 +30,7 @@ class Login(Screen):
         app.username = name
         print (f'login: name-{name} pass-{password}')
 
-        ##PUT LOGIN FUNCTION
+        ##MICHAEL-PUT LOGIN FUNCTION send her name and password and return user type (app.usertype = LOGIN(name, password))
         app.usertype = password
         if (app.usertype > 0):
             self.manager.transition = SlideTransition(direction="left")
@@ -52,8 +50,11 @@ class Login(Screen):
         self.ids['user_pass'].text = ""
 #---2---
 class ChooseDataset(Screen):
-    datasetsName = ["lymphography","adult"]
-    
+    ## MICHAEL - PUT get datasets
+
+    ## MICHAEL - datasetsNames = only array of names)
+    datasetsNames = ["lymphography","adult"]
+       
     def __init__(self, **kwargs):
         super(ChooseDataset, self).__init__(**kwargs)
         
@@ -62,21 +63,31 @@ class ChooseDataset(Screen):
             show_popup("you must choose dataset")
             return
         app = MDApp.get_running_app()
+        
+        ## MICHAEL - app.datasetname = dataset name
         app.datasetname = dataset
+        ## MICHAEL - app.datasetID = dataset ID
+        app.datasetID = 0
+
         self.manager.transition = SlideTransition(direction="left")
         self.manager.current = 'query'
-
 #---3---
 class Query(Screen):
     attributesRefs=[]
     def __init__(self, **kwargs):
         super(Query, self).__init__(**kwargs)
+        ## MICHAEL GET DATASET FEATURE DETAILS
+        ## MICHAEL ATTRIBUTES NUMBER
         attributesNumber=3
+        ## MICHAEL ATTRIBUTES NAMES
         attributesNames=["attribute1","attribute2","attribute3"]
+        ## MICHAEL ATTRIBUTES TYPES (0 - NUMERIC, 1 - CATEGORIAL)
         attributeTypes=[0,0,1]
+        ## MICHAEL CATEGORIES FOR EACH CATEGORICAL ATTRIBUTE
         attributeCategories=["cat1","cat2","cat3","cat4","cat5"]
+
         for i in range(attributesNumber):
-            print (attributesNames[i] + attributeCategories[i])
+            #print (attributesNames[i] + attributeCategories[i])
             self.ids.attributes_box.add_widget(MDLabel(text=f'{attributesNames[i]}:', halign="center"))
             if (attributeTypes[i]==0):
                 self.attributesRefs.append(MDTextFieldRect(multiline=False, hint_text=f"insert here"))
@@ -85,12 +96,22 @@ class Query(Screen):
             self.ids.attributes_box.add_widget(self.attributesRefs[i])
 
     def on_apply(self):
-        print ("query:")
+        query = []
         for i in range(len(self.attributesRefs)):
-            print (self.attributesRefs[i].text)
+            query.append(self.attributesRefs[i].text)
+        print (f'query is {query}')
+        
+        app = MDApp.get_running_app()
+        
+        ## MICHAEL- get BEST MODEL ID for model ID in app.DatasetID ,if no model return 0
+        app.model = 1
+
+        
+        ## DANA- SEND QUERY (query - arr of values, ModelID) get answer to app.results=PREDICT(query)
+        app.results= {}
+        
         self.manager.transition = SlideTransition(direction="left")
         self.manager.current = 'results'
-        
 
     def on_back(self):
         self.manager.transition = SlideTransition(direction="right")
@@ -105,23 +126,24 @@ class DataAnalystMenu(Screen):
     def chooseScreen(self, screenName):
         self.manager.transition = SlideTransition(direction="right")
         self.manager.current = screenName
-    
 #---6---
 class ManageDatasets(Screen):
-    ## LOAD FROM CONTROLLER
     def __init__(self, **kwargs):
         super(ManageDatasets, self).__init__(**kwargs)
+        ##MICHAEL - GET ALL DATASETS data = [(id, name, attribute number, dataline, timestamp),(another one),(another one)]
+        table_width = dp(Window.size[0]*9/50)
         table = MDDataTable(
-            pos_hint = {'center_x': 0.5, 'center_y': 0.5},
+            pos_hint = {'x': 0.05, 'top': 0.95},
+            size_hint= (0.9, 0.9),
             use_pagination = True,
             rows_num = 5,
             pagination_menu_height = '240dp',
             column_data = [
-                ("ID", dp (20)),
-                ("Name", dp (40)),
-                ("Attributes", dp (30)),
-                ("Instances", dp (30)),
-                ("Time stamp", dp (50)),
+                ("ID", dp (table_width*0.1)),
+                ("Name", dp (table_width*0.25)),
+                ("Attributes", dp (table_width*0.20)),
+                ("Instances", dp (table_width*0.20)),
+                ("Time stamp", dp (table_width*0.25)),
             ],
             row_data = [
                 ("1", "lymphography", "19", "148", "22-02-2023, 10:51:12"),
@@ -132,25 +154,29 @@ class ManageDatasets(Screen):
 
     def on_back(self):
         self.manager.transition = SlideTransition(direction="right")
-        self.manager.current = 'dataanalystmenu'
-          
+        self.manager.current = 'dataanalystmenu'        
 #---7---
 class CrudDatasets(Screen):
+    ##MICHAEL - GET ONE DATASET(dataset ID)
+    ##MICHAEL - SAMPLE OF UPDATE DATASET METHOD
+    ##MICHAEL - SAMPLE OF DELETE DATASET METHOd
+    ##MICHAEL - SAMPLE OF CREATE DATASET METHOd
     pass
 #---8---
 class ManageDistanceFunctions(Screen):
-    ## LOAD FROM CONTROLLER
     def __init__(self, **kwargs):
         super(ManageDistanceFunctions, self).__init__(**kwargs)
+        table_width = dp(Window.size[0]*9/50)
         table = MDDataTable(
-            pos_hint = {'center_x': 0.5, 'center_y': 0.5},
+            pos_hint = {'x': 0.05, 'top': 0.95},
+            size_hint= (0.9, 0.9),
             use_pagination = True,
             rows_num = 5,
             pagination_menu_height = '240dp',
             column_data = [
-                ("ID", dp (20)),
-                ("Name", dp (40)),
-                ("Time stamp", dp (50)),
+                ("ID", dp (table_width*0.2)),
+                ("Name", dp (table_width*0.35)),
+                ("Time stamp", dp (table_width*0.45)),
             ],
             row_data = [
                 ("1", "Hamming", "22-02-2023, 10:51:12"),
@@ -165,25 +191,34 @@ class ManageDistanceFunctions(Screen):
         self.manager.current = 'dataanalystmenu'
 #---9---
 class CrudDistanceFunctions(Screen):
+    ##MICHAEL - GET ONE DISTANCE FUNCTION(dataset ID)
+    ##MICHAEL - SAMPLE OF UPDATE DISTANCE FUNCTION METHOD
+    ##MICHAEL - SAMPLE OF DELETE DISTANCE FUNCTION METHOd
+    ##MICHAEL - SAMPLE OF CREATE DISTANCE FUNCTION METHOd
     pass
 #---10---
 class UpdateModels(Screen):
-    ## LOAD FROM CONTROLLER
     def __init__(self, **kwargs):
+        ##MICHAEL - data=GET ALL MODELS FUNCTION()
+        ##MICHAEL - datasetIDlist=ID's of ALL DATASETS FUNCTION()
+        ##MICHAEL - distancefunctionsIDlist=ID's of ALL Distance functions FUNCTION()
         super(UpdateModels, self).__init__(**kwargs)
+        table_width = dp(Window.size[0]*9/50)
         table = MDDataTable(
-            pos_hint = {'center_x': 0.5, 'center_y': 0.5},
+            pos_hint = {'x': 0.05, 'top': 0.95},
+            size_hint= (0.9, 0.9),
             check = True,
             use_pagination = True,
             rows_num = 5,
             pagination_menu_height = '240dp',
             column_data = [
-                ("ID", dp (20)),
-                ("Model Name", dp (40)),
-                ("Dataset", dp (40)),
-                ("Distance Function", dp (40)),
-                ("SSE", dp (20)),
-                ("Time stamp", dp (50)),
+                #HERE COME CHECK MARK width
+                ("ID", dp (table_width*0.1)),
+                ("Model Name", dp (table_width*0.18)),
+                ("Dataset", dp (table_width*0.18)),
+                ("Distance Function", dp (table_width*0.18)),
+                ("SSE", dp (table_width*0.1)),
+                ("Time stamp", dp (table_width*0.23)),
             ],
             row_data = [
                 ("1-1", "ly-Ha", "lymphography", "Hamming", "5.2", "22-02-2023, 10:51:12"),
@@ -207,11 +242,21 @@ class UpdateModels(Screen):
     def on_back(self):
         self.manager.transition = SlideTransition(direction="right")
         self.manager.current = 'dataanalystmenu'
+
+    ##MICHAEL - SAMPLE OF UPDATE MODEL FUNCTION METHOD
+    ##MICHAEL - SAMPLE OF DELETE DISTANCE FUNCTION METHOd
+    ##MICHAEL - SAMPLE OF CREATE DISTANCE FUNCTION METHOd
 #---11---
 class ManageUsers(Screen):
-    pass
+    def __init__(self, **kwargs):
+        super(ManageUsers, self).__init__(**kwargs)
+        ##MICHAEL - GET ALL DATASETS data = [(id, name, attribute number, dataline, timestamp),(another one),(another one)]
 #---12---
 class CrudUser(Screen):
+    ##MICHAEL - GET ONE USER(user ID)
+    ##MICHAEL - SAMPLE OF UPDATE USER METHOD
+    ##MICHAEL - SAMPLE OF DELETE USER METHOd
+    ##MICHAEL - SAMPLE OF CREATE USER METHOd
     pass
 #---13---
 class ChooseModels(Screen):
@@ -219,7 +264,7 @@ class ChooseModels(Screen):
 #---14---
 class AnalystResults(Screen):
     pass
-# WINDOW MANAGER ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~``
+# WINDOW MANAGER ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 class AnomalabApp(MDApp):
     logo = StringProperty('gui/images/logo.jpg')
     icon = StringProperty('gui/images/icon.jpg')
@@ -228,6 +273,8 @@ class AnomalabApp(MDApp):
     usertype = NumericProperty(None)
     datasetname = StringProperty(None)
     datasetID = NumericProperty(None)
+    modelID = NumericProperty(None)
+    results = {}
     def build(self):
         self.theme_cls.theme_style = "Light"
         self.theme_cls.primary_palette = "BlueGray"
@@ -262,9 +309,11 @@ class AnomalabApp(MDApp):
         WindowManager.add_widget(AnalystResults(name='analystresults'))
         
         return WindowManager
-
-## MAIN
+# MAIN ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 if __name__ == '__main__':
     app = AnomalabApp()
+    Window.fullscreen = False
+    Window.minimum_height = 600
+    Window.minimum_width = 800
     app.run()
     app.root_window.close()
