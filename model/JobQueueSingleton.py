48e3daf86cecbc528df06c3274c884b8706d3167
noamroy <80029502+noamroy@users.noreply.github.com>
2023-05-18 11:59:10 +0300

    Revert "threads-fix"
    This reverts commit e4d00153860b0f4a00dbd1d39a5a216816cd6cec.

--- model/JobQueueSingleton.py
+++ model/JobQueueSingleton.py
@@ -1,74 +1,55 @@
-import multiprocessing
+import threading
 import uuid
 import traceback
-
-import multiprocessing
-import uuid
-import traceback
-
 
 class JobQueueSingleton:
     __instance = None
-
+    
     def __new__(cls, *args, **kwargs):
         if not cls.__instance:
             cls.__instance = super(JobQueueSingleton, cls).__new__(cls, *args, **kwargs)
         return cls.__instance
-
+    
     def __init__(self):
         self.job_queue = []
-        self.manager = multiprocessing.Manager()
-        self.status = self.manager.dict()
-        self.lock = multiprocessing.Lock()
-
+        self.status = {}
+        self.lock = threading.Lock()
+    
     def add_job(self, fn, *args, **kwargs):
         job_id = str(uuid.uuid4())
-        job_process = JobProcess(job_id, fn, args, kwargs, self.status, self.lock)
+        job_thread = threading.Thread(target=self._run_job, args=(job_id, fn, args, kwargs))
         with self.lock:
-            self.job_queue.append((job_id, job_process))
+            self.job_queue.append((job_id, job_thread))
             self.status[job_id] = "queued"
-        self.status[job_id] = "running"
-        job_process.start()
+        job_thread.start()
         return job_id
-
+    
+    def _run_job(self, job_id, fn, args, kwargs):
+        try:
+            with self.lock:
+                self.status[job_id] = "running"
+            fn(*args, **kwargs)
+            with self.lock:
+                self.status[job_id] = "completed"
+        except Exception as e:
+            with self.lock:
+                self.status[job_id] = "error"
+                print (traceback.print_exc())
+                print(f"Error in job {job_id}: {e}")
+            self.status[job_id] = "completed"
+    
     def get_status(self, job_id):
         return self.status.get(job_id, None)
-
+    
     def stop_job(self, job_id):
         with self.lock:
-            for i, (jid, process) in enumerate(self.job_queue):
+            for i, (jid, thread) in enumerate(self.job_queue):
                 if jid == job_id:
-                    process.terminate()
+                    thread._stop()
                     self.job_queue.pop(i)
                     self.status[job_id] = "stopped"
                     break
-
-
-class JobProcess(multiprocessing.Process):
-    def __init__(self, job_id, fn, args, kwargs, status, lock):
-        super(JobProcess, self).__init__()
-        self.job_id = job_id
-        self.fn = fn
-        self.args = args
-        self.kwargs = kwargs
-        self.status = status
-        self.lock = lock
-
-    def run(self):
-        try:
-            with self.lock:
-                self.status[self.job_id] = "running"
-            self.fn(*self.args, **self.kwargs)
-            with self.lock:
-                self.status[self.job_id] = "completed"
-        except Exception as e:
-            with self.lock:
-                self.status[self.job_id] = "error"
-                print(traceback.print_exc())
-                print(f"Error in job {self.job_id}: {e}")
-            self.status[self.job_id] = "completed"
-
-
+    
 ## Run example with arguments
 # import modelController
 
