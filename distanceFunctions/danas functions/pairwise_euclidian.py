20a3d642332fcf5d0e0b175c45d3cd632cbaba39
einsteinium400 <48217993+einsteinium400@users.noreply.github.com>
2023-05-21 00:11:53 +0300

    refactor functions

--- distanceFunctions/danas functions/pairwise_euclidian.py
+++ distanceFunctions/danas functions/pairwise_euclidian.py
@@ -3,9 +3,6 @@ from sklearn.metrics.pairwise import euclidean_distances
 
 def pairwise_eucledean(vector1, vector2, j, k):
     distance = euclidean_distances([vector1], [vector2])
-    print ('vector1:',vector1)
-    print ('vector2:',vector2)
-    print ('distance:',distance)
     results = []
     for i in range(len(vector1)):
         results.append(0)
